267caddd573421ff339c69de078effb1297231df
Gerald <geraldelormgbagbe@gmail.com>
2026-04-23 15:44:26 +0100

    Add Script import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import AdSense from '@/components/AdSense';
+import Script from 'next/script';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import ThemeProvider from '@/components/ThemeProvider';
